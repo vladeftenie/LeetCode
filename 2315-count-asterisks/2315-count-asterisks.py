1364e4457f221ce3e6801f8f6c7ad4151beea19a
Vlad Eftenie <119301368+RagnarVKing@users.noreply.github.com>
2025-03-19 15:04:15 +0200

    Time: 3 ms (28.88%), Space: 17.7 MB (55.76%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.py
+++ 2315-count-asterisks/2315-count-asterisks.py
@@ -1,11 +1,3 @@
 class Solution:
     def countAsterisks(self, s: str) -> int:
-        read = True
-        cnt = 0
-        for c in s:
-            if c == '|':
-                read = not read
-                continue
-            if read and c == '*':
-                cnt += 1
-        return cnt
+        return sum([start.count('*') for start in s.split("|")][0::2])
